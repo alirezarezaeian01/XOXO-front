63352398ee91cab333501d149fb589dc29854e3e
Alireza Rezaeian <85349100+alirezarezaeian01@users.noreply.github.com>
2024-06-15 20:22:04 +0330

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Table from '../components/Table';
+import Login from '../components/Login';
+import HomePage from '../components/HomePage';
+import GameDesign from '../components/GameDesign';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' index element={ <App /> } />
+        <Route path='/homePage' element={ <HomePage /> } />
+        <Route path='/table' element={ <Table /> } />
+        <Route path='/login' element={ <Login /> } />
+        <Route path='/game_design' element={ <GameDesign /> } />
+      </Routes>
+    </BrowserRouter>
+
+  </React.StrictMode>
+);
